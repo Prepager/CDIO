b39781ed8ec6add5056cc48fb9677fe15239e699
Asbjørn <s153087@student.dtu.dk>
2019-06-07 14:04:56 +0200

    New version of graph
    New version of graph

--- src/sphinx/GraphTest.java
+++ src/sphinx/GraphTest.java
@@ -1,23 +1,28 @@
 package sphinx;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import java.util.*; 
+
+
 public class GraphTest extends Graph {
 
     public static void main(String args[]) {   
     	
     	GraphTest graph = new GraphTest();
     	
+    	Point robot = new Point();
+    	robot.x = 34;
+    	robot.y = 103;
+    	
         double[][] coordins = { 
         		{ 1, 2.2, 3, 5.1, 5, 6, 7, 8, 1.3, 13, 1.05, 4.1, 4.3, 4.3, 3.8, 4.8 },
                 { 1, 7, 5, 1.2, 4, 5.3, 6, 8, 6.8, 1.1, 1.05, 2.2, 4.8, 3.8, 4.3, 4.3 }, };
 
-        graph.loadCoordinates(coordins);
-        graph.createGraph();
-        graph.findClosestAll(graph.path);
+        graph.findClosest();
 
-        for (int i = 0; i < 11; i++) {
-            // java.util.print.out
-        	System.out.print("\n" + graph.path[i]);
-           // System.out.print("\n" + graph.coordinates[graph.path[i]][0]);
+        for (int i = 0; i < graph.path.size(); i++) {
+        	System.out.print("\n" + graph.path.get(i));
         }
     }
 }
