d60b3b26ee0d96e4cc2fde6c743940406d6c7796
nico4925 <nico4925@outlook.com>
2019-06-12 13:19:41 +0200

    Comments added

--- src/movement/Movement.java
+++ src/movement/Movement.java
@@ -9,56 +9,56 @@ import lejos.utility.Delay;
 import lejos.hardware.motor.JavaMotorRegulator;
 
 public class Movement {
-	
+	// Renaming motor name(s)
 	public NXTRegulatedMotor left = Motor.A;
 	public NXTRegulatedMotor right = Motor.D;
 	public NXTRegulatedMotor pickUp = Motor.B;
 	public NXTRegulatedMotor front = Motor.C;
-			
-	public void move(int speed) //setting speed
+
+	public void move(int speed) // Setting speed of vehicle
 	{
 		left.setSpeed(speed);
 		right.setSpeed(speed);
-		if(speed > 0 ) { //forward
-		left.backward();
-		right.backward();
-		} else if(speed < 0) { //backwards
+		if (speed > 0) { // Vehicle forward
+			left.backward();
+			right.backward();
+		} else if (speed < 0) { // Vehicle backwards
 			right.forward();
 			left.forward();
-		} else { //stop
+		} else { // Vehicle speed stop
 			left.stop();
 			right.stop();
 		}
 	}
-	
-	public void turn(int deg, int speed) throws InterruptedException{
+
+	public void turn(int deg, int speed) throws InterruptedException { // Turns the vehicle
 		left.setSpeed(speed);
 		right.setSpeed(speed);
-		if(deg > 0) { //right turn
+		if (deg > 0) { // Turn vehicle right
 			left.backward();
 			right.forward();
-		}else if(deg < 0) { //left turn
+		} else if (deg < 0) { // Turn vehicle left
 			left.forward();
 			right.backward();
 		}
 	}
-		
-	public void collect(int pickUpSpeed, int frontSpeed) {
+
+	public void collect(int pickUpSpeed, int frontSpeed) { // Collects/empty balls
 		pickUp.setSpeed(pickUpSpeed);
 		front.setSpeed(frontSpeed);
-		if(pickUpSpeed > 0) {
-		pickUp.forward();
-		front.forward();
-		} else if(pickUpSpeed < 0) {
+		if (pickUpSpeed > 0) { // Collects balls to vehicle
+			pickUp.forward();
+			front.forward();
+		} else if (pickUpSpeed < 0) { // Empty balls from vehicle
 			pickUp.backward();
 			front.backward();
-		} else {
+		} else { // Stop collecting balls on vehicle
 			pickUp.stop();
 			front.stop();
 		}
 	}
-	
-	public boolean isStalled() {
+
+	public boolean isStalled() { // Checks if pickUp is stalled
 		return pickUp.isStalled();
 	}
 }
--- src/movement/Server.java
+++ src/movement/Server.java
@@ -14,85 +14,78 @@ import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 import lejos.hardware.motor.JavaMotorRegulator;
 
-/**
- * A server program which accepts requests from clients to capitalize strings. When
- * a client connects, a new thread is started to handle it. Receiving client data,
- * capitalizing it, and sending the response back is all done on the thread, allowing
- * much greater throughput because more clients can be handled concurrently.
- */
 public class Server {
 
-    /**
-     * Runs the server. When a client connects, the server spawns a new thread to do
-     * the servicing and immediately returns to listening. The application limits the
-     * number of threads via a thread pool (otherwise millions of clients could cause
-     * the server to run out of resources by allocating too many threads).
-     */
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket listener = new ServerSocket(59898)) {
-            System.out.println("Server is running");
-            ExecutorService pool = Executors.newFixedThreadPool(20);
-            while (true) {
-                pool.execute(new Capitalizer(listener.accept()));
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		try (ServerSocket listener = new ServerSocket(59898)) { // Creates server socket/IP port
+			System.out.println("Server is running");
+			ExecutorService pool = Executors.newFixedThreadPool(20); // Creates thread pool
+			Sound.beep();
+			while (true) {
+				pool.execute(new serverRuntime(listener.accept())); // Creates one instance
+			}
+		}
+	}
 
-    private static class Capitalizer implements Runnable {
-        private Socket socket;
+	private static class serverRuntime implements Runnable { // Implements server logic
+		private Socket socket; // Private socket
 
-        Capitalizer(Socket socket) {
-            this.socket = socket;
-        }
+		serverRuntime(Socket socket) { // Initiate socket
+			this.socket = socket;
+		}
 
-        @Override
-        public void run() {
-        	Movement move = new Movement();
-        	Sound.beep();
-            //System.out.println("Connected: " + socket);
-        	System.out.println("Client connected");
-        	String str = "";
-            try {
-                Scanner in = new Scanner(socket.getInputStream());
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                while (!str.equals("exit")) {
-                    str = in.nextLine();
-                    out.println(str.toUpperCase());
-                    if(str != "") {
-                    String[] command = str.split(" ");
-                    switch(command[0]) {
-	                	case "move": //moves the vehicle foward, backwards and stop
-	                		move.move(Integer.parseInt(command[1]));
-	                		break;
-	                	case "turn": //turns the vehicle left or right
-	                		move.turn(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
-	                		break;
-	                	case "stop": //stops the vehicle
-	                		move.move(0);
-	                	case "collect": //turns the pickup mekanism on
-	                		move.collect(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
-	                		break;
-	                	default: //stops movement of the car
-	                		move.move(0);
-	                		System.out.println("error command: "+ command[0]); //command does not excist
-	                		break;
-	                    }
-                    }
-                    if(move.isStalled()) {out.println("stalled");} //checks of motor is stalled
-                }
-                in.close();
-                out.close();
-            } catch (Exception e) {
-            	System.out.println("Client disconnected");
-                //System.out.println("Error:" + socket);
-            } finally {
-                try { socket.close(); } catch (IOException e) {
-                	System.out.println("Socket didn't close properly");
-                	//System.out.println("Closed: " + socket);
-                }
-                move.move(0);
-                move.collect(0,0);
-            }
-        }
-    }
+		@Override
+		public void run() { // Runs server
+			Movement move = new Movement(); // Creates instance of motor logic
+			Sound.beep(); // Client connect notification
+			// System.out.println("Connected: " + socket);
+			System.out.println("Client connected");
+			String str = "";
+			try {
+				Scanner in = new Scanner(socket.getInputStream()); // Instance of input (client commands)
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // Instance of output (to client)
+				while (!str.equals("exit")) { // While client has not exited
+					str = in.nextLine(); // Reads new command from client
+					// out.println(str.toUpperCase()); //sends back command to client
+					if (str != "") {
+						String[] command = str.split(" ");
+						switch (command[0]) {
+						case "move": // Moves the vehicle forward, backwards and stop
+							move.move(Integer.parseInt(command[1]));
+							break;
+						case "turn": // Turns the vehicle left or right
+							move.turn(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+							break;
+						case "stop": // Stops the vehicle
+							move.move(0);
+						case "collect": // Turns the pickup mekanism on/off
+							move.collect(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+							break;
+						default: // Stops movement of the car
+							move.move(0);
+							System.out.println("error command: " + command[0]); // Command does not excist
+							break;
+						}
+					}
+					if (move.isStalled()) { // Checks of motor is stalled
+						out.println("stalled");
+					}
+				}
+				in.close(); // Closes input
+				out.close(); // Closes output
+			} catch (Exception e) { // Client disconnect
+				System.out.println("Client disconnected");
+				// System.out.println("Error:" + socket);
+			} finally { // Tries to close socket
+				try {
+					socket.close();
+				} catch (IOException e) {
+					System.out.println("Socket didn't close properly");
+					// System.out.println("Closed: " + socket);
+				}
+				move.move(0); // Stops movement
+				move.collect(0, 0); // Stop collection
+			}
+		}
+	}
 }
