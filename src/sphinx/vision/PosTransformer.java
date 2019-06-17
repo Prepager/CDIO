41ff682c8cad70479f58eb2816af52222e8bd801
Uniel <adam@saunte.dk>
2019-06-17 15:08:22 +0200

    Updated position transformation for car, to reflect smaller triangle

--- src/sphinx/vision/PosTransformer.java
+++ src/sphinx/vision/PosTransformer.java
@@ -2,7 +2,10 @@ package sphinx.vision;
 
 import java.util.Arrays;
 
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 public class PosTransformer {
 
@@ -63,7 +66,7 @@ public class PosTransformer {
 		
 		// Move coordinate system to move center to origo.
 		Point[] transformedObjectPosition = Arrays.copyOf(
-			this.transformCoordinateSystem(objectPosition),
+			this.transformCoordinateSystem(objectPosition, this.center),
 			objectPosition.length
 		);
 		
@@ -77,7 +80,43 @@ public class PosTransformer {
 		}
 		
 		// Return to old coordinate system.
-		return this.inverseTransformCoordinateSystem(transformedObjectPosition);
+		transformedObjectPosition = this.inverseTransformCoordinateSystem(transformedObjectPosition, this.center);
+		
+		// Find center of triangle
+		Point objectCenter = this.findCenter(new MatOfPoint2f(transformedObjectPosition));
+//		Point objectCenter = findCenter(transformedObjectPosition);
+
+		
+		// Transform to coordinate system with triangle center as origo.
+		transformedObjectPosition = this.transformCoordinateSystem(transformedObjectPosition, objectCenter);
+		// Scale according to real-life factors
+		for(int i = 0; i < transformedObjectPosition.length; i++) {
+			transformedObjectPosition[i].set(new double[] {
+				transformedObjectPosition[i].x * 2,
+				transformedObjectPosition[i].y * 2
+			});
+		}
+		
+		return this.inverseTransformCoordinateSystem(transformedObjectPosition, objectCenter);
+	}
+	
+	/**
+	 * Find the center point of the triangle contour.
+	 *
+	 * @param triangle
+	 * @return Point
+	 */
+	private Point findCenter(MatOfPoint2f triangle) {
+		// Find moments for the triangle.
+		Moments moments = Imgproc.moments(triangle);
+		
+		// Create point and set x, and y positions.
+		Point center = new Point();
+		center.x = moments.get_m10() / moments.get_m00();
+		center.y = moments.get_m01() / moments.get_m00();
+		
+		// Return the found center point.
+		return center;
 	}
 
 	/**
@@ -85,12 +124,12 @@ public class PosTransformer {
 	 *
 	 * @param Point[] the points, that defines the given geometric object
 	 */	
-	private Point[] inverseTransformCoordinateSystem(Point[] objectPosition) {
+	private Point[] inverseTransformCoordinateSystem(Point[] objectPosition, Point centerOffset) {
 		Point[] transformedObjectPosition = objectPosition;
 		for(int i = 0; i < objectPosition.length; i++) {
 			objectPosition[i].set(new double[] {
-				objectPosition[i].x += center.x,
-				objectPosition[i].y += center.y					
+				objectPosition[i].x += centerOffset.x,
+				objectPosition[i].y += centerOffset.y					
 			});
 		}
 		
@@ -102,13 +141,13 @@ public class PosTransformer {
 	 *
 	 * @param Point[] the points, that defines the given geometric object
 	 */	
-	private Point[] transformCoordinateSystem(Point[] objectPosition) {
+	private Point[] transformCoordinateSystem(Point[] objectPosition, Point newCenter) {
 		Point[] transformedObjectPosition = objectPosition;
 		for(int i = 0; i < objectPosition.length; i++) {
 			// Move location to coordinate system with center in origo 
 			transformedObjectPosition[i].set(new double[] {
-				objectPosition[i].x - center.x,
-				objectPosition[i].y - center.y
+				objectPosition[i].x - newCenter.x,
+				objectPosition[i].y - newCenter.y
 			});
 		}
 		
